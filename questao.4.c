32c0112e7b9a731cdc530111a6d0c00cdb2b10ce
Gildson <37073818+Gildson@users.noreply.github.com>
2018-08-24 11:21:01 -0300

    Add files via upload

---
+++ questao.4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i, j;
+    int *p;
+    int *q;
+
+    printf("%d", p = i);
+    printf("\n");
+    printf("%d", q = &j);
+    printf("\n");
+    printf("%d", p = &*&i);
+    printf("\n");
+    //printf("%d", i = (*&)j);  //expressão de atribuição ilegal
+    printf("%d", i = *&j);
+    printf("\n");
+    printf("%d", i = *&*&j);
+    printf("\n");
+    printf("%d", q = *p);
+    printf("\n");
+    printf("%d", i = (*p)++ + *q);
+
+    return 0;
+}
