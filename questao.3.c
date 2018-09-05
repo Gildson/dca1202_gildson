e13a2b682073cb8aa4f635cd701f058adb5eb78c
Gildson <37073818+Gildson@users.noreply.github.com>
2018-09-05 20:15:46 -0300

    Update questao.3.c
    formatação das respostas

--- questao.3.c
+++ questao.3.c
@@ -7,6 +7,7 @@ int main()
     p = &i;
     printf("%x %d %d %d %d", p,*p+2,**&p,3**p,**&p+4);
 
+    //supondo que i ocupe o endereco 4094 na memoria, o que sera impresso na tela sao os seguintes resultados 
     //b7186cbc   7   5    15   9
 
     return 0;
